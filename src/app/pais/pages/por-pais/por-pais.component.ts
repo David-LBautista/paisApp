3caf809ad24c30a519ba16066a5080458a07a359
David LB <48845853+David-LBautista@users.noreply.github.com>
2021-03-04 21:36:18 -0600

    Caja de sugerencias para el inputcomponent

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -12,6 +12,8 @@ export class PorPaisComponent implements OnInit {
   termino:string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+
 
   constructor(
     private paisService: PaisService
@@ -36,7 +38,12 @@ export class PorPaisComponent implements OnInit {
 
   sugerencias( termino:string ){
     this.hayError = false;
-    
+    this.paisService.buscarPais( termino )
+    .subscribe( paises => {
+
+      this.paisesSugeridos = paises.splice(0, 5)
+      
+    }, (err) => this.paisesSugeridos = [])
     
   }
 
